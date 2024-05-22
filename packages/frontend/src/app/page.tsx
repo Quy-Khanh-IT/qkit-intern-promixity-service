5edda0cc5d7c0f8837214138716491356b091e41
21127197 <ndtuan21@clc.fitus.edu.vn>
2024-05-22 15:45:28 +0700

    refactor: rename mistake files (ver 2)

--- packages/frontend/src/app/page.tsx
+++ packages/frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import Footer from '@/app/components/Footer'
 import HeroSection from '@/app/components/HeroSection'
-import NavBar from '@/app/components/NavBar'
+import Header from '@/app/components/Header'
 import ServiceSlider from '@/app/components/ServiceSlider'
 import StarsCanvas from './components/canvas/Star'
 import CredibilitySection from './components/CredibilitySection'
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <main>
       <ProcessBar />
-      <NavBar />
+      <Header />
       <div style={{ position: 'relative', zIndex: 0 }} className='scroll-bar-1'>
         <HeroSection />
         <CredibilitySection />
