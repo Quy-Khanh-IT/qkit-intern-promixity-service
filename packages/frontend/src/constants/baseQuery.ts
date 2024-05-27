404570e12b3546f4214779fe4535dce300ac83b5
21127197 <ndtuan21@clc.fitus.edu.vn>
2024-05-28 00:10:07 +0700

    merge FrontEnd 28/5

--- packages/frontend/src/constants/baseQuery.ts
+++ packages/frontend/src/constants/baseQuery.ts
@@ -1,4 +1,3 @@
-import { fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { HttpStatusCode } from 'axios'
 import { API_ENDPOINT, ROUTE, StorageKey } from '@/constants'
 import { getFromLocalStorage } from '@/utils/storage.util'
