27329e853be57b0517fb403a94f940b304cdbd8f
21127197 <ndtuan21@clc.fitus.edu.vn>
2024-05-30 17:51:13 +0700

    fix: refactor light.ts

--- packages/frontend/src/configs/themes/light.ts
+++ packages/frontend/src/configs/themes/light.ts
@@ -1,23 +1,15 @@
 import variables from '@/sass/common/_variables.module.scss'
 import { ThemeConfig } from 'antd'
-import { Nunito } from 'next/font/google'
 const { mainColor, light, dark, subColor, subColor2, grayRowHover, errorColor } = variables
-
-const _nunitoFont = Nunito({
-  subsets: ['latin'],
-  weight: ['300', '400', '500', '700', '900']
-})
 
 export const themConfig: ThemeConfig = {
   token: {
-    // Seed Token
     colorPrimary: mainColor,
     colorText: dark,
     colorError: errorColor,
     fontSize: 16,
     fontFamily: 'Nunito, sans-serif',
 
-    // Alias Token
     colorBgContainer: light
   },
   components: {
@@ -33,31 +25,4 @@ export const themConfig: ThemeConfig = {
       bodySortBg: subColor2
     }
   }
-  // components: {
-  //   Input: {
-  //     colorBgContainerDisabled: disabledBackground,
-  //     activeShadow: 'none',
-  //     colorTextDisabled: '#333'
-  //   },
-  //   Select: {
-  //     boxShadow: 'none',
-  //     controlOutlineWidth: 0,
-  //     optionSelectedBg: subColor
-  //   },
-  //   Menu: {
-  //     itemSelectedBg: mainColor,
-  //     itemSelectedColor: light
-  //   },
-  //   Table: {
-  //     headerBg: subColor
-  //   },
-  //   List: {
-  //     lineWidth: 0
-  //   },
-  //   Button: {
-  //     primaryShadow: 'none'
-  //   },
-  //   Image: {},
-  //   Typography: {}
-  // }
 }
