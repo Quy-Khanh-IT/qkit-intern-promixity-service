8e9bbd4da083d6258a40b7183f2a7bb997d19ff3
ninehcobra <ttbexinhtt2903@gmail.com>
2024-06-01 19:30:04 +0700

    update business type

--- packages/frontend/src/types/business.ts
+++ packages/frontend/src/types/business.ts
@@ -1,8 +1,60 @@
 export interface IBusiness {
   name: string
-  category: string[]
-  address: string
-  totalReviews: number
+  description: string
+  phoneNumber: string
+  website: string
+  category: ICategory[]
+  services: IService[]
   overallRating: number
-  status: string
+  totalReview: number
+  addressLine: string
+  fullAddress: string
+  province: string
+  district: string
+  country: string
+  dayOfWeek: IDayOfWeek[]
+  userId: string
+  images: IBusinessImage[]
+  stars: IStar[]
+  created_at: string
+  updated_at: string
+  deleted_at: string | null
+  score: number
+  id: string
+  _distance: number
+}
+
+export interface IStar {
+  star: string
+  count: number
+}
+
+export interface ICategory {
+  name: string
+  id: string
+}
+
+export interface IBusinessImage {
+  url: string
+  public_id: string
+  etag: string
+  phash: string
+}
+
+export interface IDayOfWeek {
+  day: string
+  openTime: string
+  closeTime: string
+  order: number
+}
+
+export interface IBusinessCategory {
+  name: string
+  id: string
+}
+
+export interface IService {
+  name: string
+  id: string
+  order: number
 }
