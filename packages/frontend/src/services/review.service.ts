2f613bc8580a337ebd62fb7617c5a0b2cb37d1a6
Nguyen Duc Tuan <ndtuan21@clc.fitus.edu.vn>
2024-06-21 17:57:30 +0700

    fix: merge

--- packages/frontend/src/services/review.service.ts
+++ packages/frontend/src/services/review.service.ts
@@ -102,7 +102,6 @@ export const {
   useCreateResponseCommentMutation,
   useGetReviewsForAdminQuery,
   useGetEmotionsQuery,
-  useGetReviewsByIdQuery
   useGetReviewsByIdQuery,
   useDeleteReviewByIdMutation,
   useDeleteCommentByIdMutation,
